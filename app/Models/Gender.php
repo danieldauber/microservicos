9cc0bd1d2b88fec2a0da141b99ad7ce42bacf158
Daniel Dauber <ddauberox@gmail.com>
2021-02-24 20:05:45 -0300

    criando recurso caregory

---
+++ app/Models/Gender.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Traits\Uuid as TraitsUuid;
+
+class Gender extends Model
+{
+    use SoftDeletes, TraitsUuid;
+
+    protected $fillable = ['name', 'is_active'];
+    protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string'
+    ];
+}
