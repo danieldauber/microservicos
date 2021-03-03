ab3a86db9e7f5578a5ca86e85e1c582cd507f241
Daniel Dauber <ddauberox@gmail.com>
2021-03-03 19:33:54 -0300

    teste http

---
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryControllerTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+
+        $category = factory(Category::class)->create();
+
+        $response = $this->get(route('categories.index'));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([$category->toArray()]);
+    }
+
+    public function testShow()
+    {
+
+        $category = factory(Category::class)->create();
+
+        $response = $this->get(route('categories.show', ['category' => $category->id]));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson($category->toArray());
+    }
+
+    public function testeInvalidationData()
+    {
+        $response = $this->json('POST', route('categories.store'), []);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name'])
+            ->assertJsonMissingValidationErrors(['is_active'])
+            ->assertJsonFragment([
+                \Lang::get('validation.required', ['attribute' => 'name'])
+            ]);
+
+        $response = $this->json('POST', route('categories.store'), [
+            'name' => str_repeat('a', 256),
+            'is_active' => 'a'
+        ]);
+
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name', 'is_active'])
+            ->assertJsonFragment([
+                \Lang::get('validation.max.string', ['attribute' => 'name', 'max' => 255])
+            ])
+            ->assertJsonFragment([
+                \Lang::get('validation.boolean', ['attribute' => 'is active'])
+            ]);
+
+        $category = factory(Category::class)->create();
+
+        $response = $this->json(
+            'PUT',
+            route('categories.update', ['category' => $category->id]),
+            [
+                'name' => str_repeat('a', 256),
+                'is_active' => 'a'
+            ]
+        );
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name', 'is_active'])
+            ->assertJsonFragment([
+                \Lang::get('validation.max.string', ['attribute' => 'name', 'max' => 255])
+            ])
+            ->assertJsonFragment([
+                \Lang::get('validation.boolean', ['attribute' => 'is active'])
+            ]);
+    }
+
+    public function testStore()
+    {
+
+        $response = $this->json('POST', route('categories.store'), [
+            'name' => 'test',
+        ]);
+
+        $category = Category::find($response->json('id'));
+        $response
+            ->assertStatus(201)
+            ->assertJson($category->toArray());
+
+        $this->assertTrue($response->json('is_active'));
+        $this->assertNull($response->json('description'));
+
+        $response = $this->json('POST', route('categories.store'), [
+            'name' => 'test',
+            'description' => 'description',
+            'is_active' => false
+        ]);
+
+        $response->assertJsonFragment([
+            'description' => 'description',
+            'is_active' => false
+        ]);
+    }
+
+    public function testUpdate()
+    {
+
+        $category = factory(Category::class)->create([
+            'is_active' => false,
+            'description' => 'description'
+        ]);
+
+        $response = $this->json(
+            'PUT',
+            route('categories.update', ['category' => $category->id]),
+            [
+                'name' => 'test',
+                'is_active' => true,
+                'description' => 'teste'
+            ]
+        );
+
+        $category = Category::find($response->json('id'));
+        $response
+            ->assertStatus(200)
+            ->assertJson($category->toArray())
+            ->assertJsonFragment([
+                'description' => 'teste',
+                'is_active' => true,
+                'name' => 'test'
+            ]);
+    }
+}
---
+++ tests/Feature/Http/Controllers/Api/GenderControllerTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use App\Models\Gender;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GenderControllerTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+
+        $gender = factory(Gender::class)->create();
+
+        $response = $this->get(route('genders.index'));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([$gender->toArray()]);
+    }
+
+    public function testShow()
+    {
+
+        $gender = factory(Gender::class)->create();
+
+        $response = $this->get(route('genders.show', ['gender' => $gender->id]));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson($gender->toArray());
+    }
+
+    public function testeInvalidationData()
+    {
+        $response = $this->json('POST', route('genders.store'), []);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name'])
+            ->assertJsonMissingValidationErrors(['is_active'])
+            ->assertJsonFragment([
+                \Lang::get('validation.required', ['attribute' => 'name'])
+            ]);
+
+        $response = $this->json('POST', route('genders.store'), [
+            'name' => str_repeat('a', 256),
+            'is_active' => 'a'
+        ]);
+
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name', 'is_active'])
+            ->assertJsonFragment([
+                \Lang::get('validation.max.string', ['attribute' => 'name', 'max' => 255])
+            ])
+            ->assertJsonFragment([
+                \Lang::get('validation.boolean', ['attribute' => 'is active'])
+            ]);
+
+        $gender = factory(Gender::class)->create();
+
+        $response = $this->json(
+            'PUT',
+            route('genders.update', ['gender' => $gender->id]),
+            [
+                'name' => str_repeat('a', 256),
+                'is_active' => 'a'
+            ]
+        );
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name', 'is_active'])
+            ->assertJsonFragment([
+                \Lang::get('validation.max.string', ['attribute' => 'name', 'max' => 255])
+            ])
+            ->assertJsonFragment([
+                \Lang::get('validation.boolean', ['attribute' => 'is active'])
+            ]);
+    }
+
+    public function testStore()
+    {
+
+        $response = $this->json('POST', route('genders.store'), [
+            'name' => 'test',
+        ]);
+
+        $gender = gender::find($response->json('id'));
+        $response
+            ->assertStatus(201)
+            ->assertJson($gender->toArray());
+
+        $this->assertTrue($response->json('is_active'));
+
+        $response = $this->json('POST', route('genders.store'), [
+            'name' => 'test',
+            'is_active' => false
+        ]);
+
+        $response->assertJsonFragment([
+            'is_active' => false
+        ]);
+    }
+
+    public function testUpdate()
+    {
+
+        $gender = factory(Gender::class)->create([
+            'is_active' => false,
+        ]);
+
+        $response = $this->json(
+            'PUT',
+            route('genders.update', ['gender' => $gender->id]),
+            [
+                'name' => 'test',
+                'is_active' => true,
+            ]
+        );
+
+        $gender = gender::find($response->json('id'));
+        $response
+            ->assertStatus(200)
+            ->assertJson($gender->toArray())
+            ->assertJsonFragment([
+                'is_active' => true,
+                'name' => 'test'
+            ]);
+    }
+}
