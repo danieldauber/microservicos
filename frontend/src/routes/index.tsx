e83361a736e9a3c95446575da1b8b02afa9faea9
Daniel Dauber <ddauberox@gmail.com>
2021-08-10 23:30:04 -0300

    crud category

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { RouteProps } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import CategoryList from "../pages/category/PageList";
+import CategoryForm from "../pages/category/PageForm";
+
 import CastMemberList from "../pages/cast-member/PageList";
 import GenreList from "../pages/genre/PageList";
 
@@ -28,7 +30,7 @@ const routes: MyRouteProps[] = [
     name: "categories.create",
     label: "Criar categoria",
     path: "/categories/create",
-    component: CategoryList,
+    component: CategoryForm,
     exact: true,
   },
   {
