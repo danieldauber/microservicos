60938c66df534f027fca70c8d94f34d15bc9f12d
Daniel Dauber <ddauberox@gmail.com>
2021-08-16 22:33:39 -0300

    add graphql

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -52,4 +52,6 @@ $app->singleton(
 |
 */
 
+// $app->register(\Nuwave\Lighthouse\LighthouseServiceProvider::class);
+
 return $app;
--- backend/bootstrap/cache/packages.php
+++ backend/bootstrap/cache/packages.php
@@ -31,6 +31,13 @@
       0 => 'Laravel\\Tinker\\TinkerServiceProvider',
     ),
   ),
+  'mll-lab/laravel-graphql-playground' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'MLL\\GraphQLPlayground\\GraphQLPlaygroundServiceProvider',
+    ),
+  ),
   'nesbot/carbon' => 
   array (
     'providers' => 
@@ -43,6 +50,23 @@
     'providers' => 
     array (
       0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nuwave/lighthouse' => 
+  array (
+    'aliases' => 
+    array (
+      'graphql' => 'Nuwave\\Lighthouse\\GraphQL',
+    ),
+    'providers' => 
+    array (
+      0 => 'Nuwave\\Lighthouse\\LighthouseServiceProvider',
+      1 => 'Nuwave\\Lighthouse\\GlobalId\\GlobalIdServiceProvider',
+      2 => 'Nuwave\\Lighthouse\\OrderBy\\OrderByServiceProvider',
+      3 => 'Nuwave\\Lighthouse\\Pagination\\PaginationServiceProvider',
+      4 => 'Nuwave\\Lighthouse\\Scout\\ScoutServiceProvider',
+      5 => 'Nuwave\\Lighthouse\\SoftDeletes\\SoftDeletesServiceProvider',
+      6 => 'Nuwave\\Lighthouse\\Validation\\ValidationServiceProvider',
     ),
   ),
 );
